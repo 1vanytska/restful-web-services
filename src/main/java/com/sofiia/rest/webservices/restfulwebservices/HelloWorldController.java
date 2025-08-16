0df5b1db148b73aeeb84ab3e46bbab7640317029
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2025-08-16 19:55:27 +0300

    Connecting Angular Frontend with Restful API

--- src/main/java/com/sofiia/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/sofiia/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,4 +1,31 @@
 package com.sofiia.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.*;
+
+//Controller
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
 public class HelloWorldController {
+
+    //GET
+    //URI - /hello-world
+    // method - "Hello World"
+    // @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    //hello-world-bean
+    @GetMapping(path = "hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+//        throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
+        return new HelloWorldBean("Hello World - Changed");
+    }
+
+    //hello-world/path-variable/sofiia
+    @GetMapping(path = "hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
 }
