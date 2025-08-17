d91d796e78d4a69127fee73d25c7ff4d9844a091
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2025-08-17 12:35:19 +0300

    Create DELETE Request Method

--- src/main/java/com/sofiia/rest/webservices/restfulwebservices/TodoResource.java
+++ src/main/java/com/sofiia/rest/webservices/restfulwebservices/TodoResource.java
@@ -3,10 +3,8 @@ package com.sofiia.rest.webservices.restfulwebservices;
 import com.sofiia.rest.webservices.restfulwebservices.todo.Todo;
 import com.sofiia.rest.webservices.restfulwebservices.todo.TodoHardcodedService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +19,18 @@ public class TodoResource {
     public List<Todo> getAllTodo(@PathVariable String username){
         return todoService.findAll();
     }
+
+    //DELETE /users/{username}/todos/{id}
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(
+            @PathVariable String username, @PathVariable long id) {
+
+        Todo todo = todoService.deleteById(id);
+        if(todo!=null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
