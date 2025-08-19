435b698f8e9407719399a8f7cb84e7f380021f72
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2025-08-19 11:32:40 +0300

    Creating REST API for Creating a Todo - POST Request Method

--- src/main/java/com/sofiia/rest/webservices/restfulwebservices/TodoResource.java
+++ src/main/java/com/sofiia/rest/webservices/restfulwebservices/TodoResource.java
@@ -3,9 +3,12 @@ package com.sofiia.rest.webservices.restfulwebservices;
 import com.sofiia.rest.webservices.restfulwebservices.todo.Todo;
 import com.sofiia.rest.webservices.restfulwebservices.todo.TodoHardcodedService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @CrossOrigin(origins="http://localhost:4200")
@@ -18,6 +21,11 @@ public class TodoResource {
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodo(@PathVariable String username){
         return todoService.findAll();
+    }
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo getTodo(@PathVariable String username, @PathVariable long id){
+        return todoService.findById(id);
     }
 
     //DELETE /users/{username}/todos/{id}
@@ -33,4 +41,30 @@ public class TodoResource {
         return ResponseEntity.notFound().build();
     }
 
+    //Edit/Update a Todo
+    //PUT /users/{user_name}/todos/{todo_id}
+    @PutMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(
+            @PathVariable String username,
+            @PathVariable long id, @RequestBody Todo todo) {
+        Todo todoUpdated = todoService.save(todo);
+        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+    }
+
+    //Create a new Todo
+    //POST /users/{user_name)/todos/
+    @PostMapping("/users/{username}/todos")
+    public ResponseEntity<Void> cteateTodo(
+            @PathVariable String username, @RequestBody Todo todo) {
+        Todo createdTodo = todoService.save(todo);
+
+        //Location
+        //Get current resource url
+        // /{id}
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(createdTodo.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
 }
